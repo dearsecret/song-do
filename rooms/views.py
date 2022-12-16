411ff44a5d9f4b1881b1622178ef58f0c953f152
dearsecret <dearsecret1217@gmail.com>
2022-12-16 19:04:36 +0900

    detail rest api

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from .serializers import KindSerializer, FacilitySerializer
 from .models import Kind, Facility
 
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_204_NO_CONTENT
+from rest_framework.exceptions import NotFound
 
 
 class Kinds(APIView):
@@ -23,6 +24,32 @@ class Kinds(APIView):
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
+class KindDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Kind.objects.get(pk=pk)
+        except Kind.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        kind = self.get_object(pk)
+        serializer = KindSerializer(kind)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = KindSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            kind = serializer.save()
+            return Response(KindSerializer(kind).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        kind = self.get_object(pk)
+        kind.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
 class Facilities(APIView):
     # authentication_classes
 
@@ -38,3 +65,28 @@ class Facilities(APIView):
             return Response(FacilitySerializer(new_facility).data)
         else:
             return Response(serializer.errors)
+
+
+class FacilityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Facility.objects.get(pk=pk)
+        except Facility.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        facility = self.get_object(pk)
+        return Response(FacilitySerializer(facility).data)
+
+    def put(self, request, pk):
+        serializer = FacilitySerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            facility = serializer.save()
+            return Response(FacilitySerializer(facility).data)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        facility = self.get_object(pk)
+        facility.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
