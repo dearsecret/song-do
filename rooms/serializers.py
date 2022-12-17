8cd3621a40d5e57bad2690328f42316bfe4926fa
dearsecret <dearsecret1217@gmail.com>
2022-12-17 15:24:35 +0900

    DetailSerializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -16,10 +16,40 @@ class FacilitySerializer(serializers.ModelSerializer):
 
 
 class RoomSerializer(serializers.ModelSerializer):
+    kind = KindSerializer(read_only=True)
+    host = PrivateUserSerializer(read_only=True)
+    is_owner = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = (
+            "name",
+            "city",
+            "kind",
+            "deposit",
+            "host",
+            "is_owner",
+        )
+
+    def get_is_owner(self, room):
+        request = self.context.get("request")
+        if request:
+            return request.user == room.owner
+        return False
+
+
+class RoomDetailSerializer(serializers.ModelSerializer):
     facilities = FacilitySerializer(many=True, read_only=True)
     kind = KindSerializer(read_only=True)
     host = PrivateUserSerializer(read_only=True)
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
+
+    def get_is_owner(self, room):
+        request = self.context.get("request")
+        if request:
+            return request.user == room.owner
+        return False
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import KindSerializer, FacilitySerializer , RoomSerializer
-from .models import Kind, Facility , Room
+from .serializers import (
+    KindSerializer,
+    FacilitySerializer,
+    RoomSerializer,
+    RoomDetailSerializer,
+)
+from .models import Kind, Facility, Room
 
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_204_NO_CONTENT
 from rest_framework.exceptions import NotFound
@@ -93,10 +98,24 @@ class FacilityDetail(APIView):
         facility.delete()
         return Response(status=HTTP_204_NO_CONTENT)
 
+
 class Rooms(APIView):
     # authentication_classes
 
     def get(self, request):
         rooms = Room.objects.all()
         serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+
+
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        serializer = RoomDetailSerializer(room, context={"request", request})
         return Response(serializer.data)
